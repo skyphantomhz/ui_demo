f01c13d0c7ba944ea1b7f790a7ac7ac523d2150c
Tran Dac Tri <tri.trandac@axonactive.com>
2020-02-26 17:24:05 +0700

    Add page four

--- lib/ui/page_four.dart
+++ lib/ui/page_four.dart
@@ -8,10 +8,74 @@ class PageFour extends StatefulWidget {
 }
 
 class _PageFourState extends State<PageFour> {
+  List<Character> _characters;
+  PageController _controller;
+
+  @override
+  void initState() {
+    _characters = List<Character>()
+      ..add(Character("Uzumaki Naruto", "assets/images/image_1.png"))
+      ..add(Character("Uchiha Sasuke", "assets/images/image_2.png"))
+      ..add(Character("Sakura Haruno", "assets/images/image_1.png"))
+      ..add(Character("Shikamaru Nara", "assets/images/image_2.png"))
+      ..add(Character("Kakashi Hatake", "assets/images/image_1.png"));
+
+      _controller = PageController(viewportFraction: 0.7);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: SafeArea(child: Container()),
+      body: SafeArea(
+        child: Container(
+          height:400,
+          child: PageView.builder(
+            itemCount: _characters.length,
+            itemBuilder: (context, index) {
+              final item = _characters[index];
+              return _buildCard(item);
+            },
+            controller: _controller,
+          ),
+        ),
+      ),
     );
   }
+
+  Widget _buildCard(Character character) {
+    return Card(
+        elevation: 8,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  gradient: LinearGradient(colors: [
+                    Colors.black.withAlpha(100),
+                    Colors.black.withAlpha(100)
+                  ])),
+              child: Image.asset(character.url),
+            ),
+            Align(
+              alignment: Alignment(0, -1),
+              child: Text(
+                character.name,
+                style: Theme.of(context)
+                    .textTheme
+                    .title
+                    .copyWith(color: Colors.white),
+              ),
+            )
+          ],
+        ),
+    
+    );
+  }
+}
+
+class Character {
+  Character(this.name, this.url);
+  String name;
+  String url;
 }
